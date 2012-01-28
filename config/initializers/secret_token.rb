1da1378b002743d1a7876a47027f524ff7ac0e47
Robert A. Iannucci Jr <robbie@rail.com>
2012-01-28 02:22:31 -0800

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChirpdIt::Application.config.secret_token = '8a02c71d28aacb0126bc98d608d0b2e35ebd039daa82837a1cfd26dc727eea895dd2e1a104b06b01b5c6342385b03b852d6a0729d3bd33c7982e1c7aff9ed4ce'
